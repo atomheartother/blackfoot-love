ed747864254d0f466fee2869eeae214c9e296586
atomheartother <atomheartother@gmail.com>
2020-02-16 23:37:03 +0100

    First push

---
+++ engine/electron/preload.js
@@ -0,0 +1,10 @@
+/* global require */
+const { ipcRenderer } = require ('electron');
+
+window.ipcRendererSend = function (...args) {
+	return ipcRenderer.send (...args);
+};
+
+window.ipcRendererReceive = function (...args) {
+	return ipcRenderer.on (...args);
+};
---
+++ js/script.js
@@ -0,0 +1,136 @@
+/* global monogatari */
+/* global characters */
+
+// Define the messages used in the game.
+monogatari.action ('message').messages ({
+	'Help': {
+		title: 'Help',
+		subtitle: 'Some useful Links',
+		body: `
+			<p><a href='https://developers.monogatari.io/documentation/'>Documentation</a> - Everything you need to know.</p>
+			<p><a href='https://monogatari.io/demo/'>Demo</a> - A simple Demo.</p>
+		`
+	}
+});
+
+// Define the notifications used in the game
+monogatari.action ('notification').notifications ({
+});
+
+// Credits of the people involved in the creation of this awesome game
+monogatari.configuration ('credits', {
+
+});
+
+// Define the Particles JS Configurations used in the game
+monogatari.action ('particles').particles ({
+
+});
+
+// Define the images that will be available on your game's image gallery
+monogatari.assets ('gallery', {
+
+});
+
+// Define the music used in the game.
+monogatari.assets ('music', {
+
+});
+
+// Define the voice files used in the game.
+monogatari.assets ('voices', {
+
+});
+
+// Define the sounds used in the game.
+monogatari.assets ('sounds', {
+
+});
+
+// Define the videos used in the game.
+monogatari.assets ('videos', {
+
+});
+
+// Define the images used in the game.
+monogatari.assets ('images', {
+
+});
+
+// Define the backgrounds for each scene.
+monogatari.assets ('scenes', {
+
+});
+
+
+// Define the Characters
+monogatari.characters ({
+	'pm': {
+		color: 'red'
+	}
+});
+
+monogatari.script ({
+	// The game starts here.
+	'Start': [
+		"Ça y est, c'est mon premier jour dans ma nouvelle école... Et j'ai",
+		"On est en plein milieu d'année, donc",
+		"",
+		{
+			'Input': {
+				'Text': 'What is your name?',
+				'Validation': function (input) {
+					return input.trim ().length > 0;
+				},
+				'Save': function (input) {
+					this.storage ({
+						player: {
+							name: input
+						}
+					});
+					return true;
+				},
+				'Revert': function () {
+					this.storage ({
+						player: {
+							name: ''
+						}
+					});
+				},
+				'Warning': 'You must enter a name!'
+			}
+		},
+		'y Hi {{player.name}} Welcome to Monogatari!',
+		{
+			'Choice': {
+				'Dialog': 'y Have you already read some documentation?',
+				'Yes': {
+					'Text': 'Yes',
+					'Do': 'jump Yes'
+				},
+				'No': {
+					'Text': 'No',
+					'Do': 'jump No'
+				}
+			}
+		}
+	],
+
+	'Yes': [
+		'y Thats awesome!',
+		'y Then you are ready to go ahead and create an amazing Game!',
+		'y I can’t wait to see what story you’ll tell!',
+		'end'
+	],
+
+	'No': [
+
+		'y You can do it now.',
+
+		'show message Help',
+
+		'y Go ahead and create an amazing Game!',
+		'y I can’t wait to see what story you’ll tell!',
+		'end'
+	]
+});
